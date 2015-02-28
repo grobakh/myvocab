08773e1f9ad946047235844417a242c41cf42980
grobakh <grobakh@gmail.com>
2015-02-28 03:14:57 +0300

    json dict

--- src/previewDict.js
+++ src/previewDict.js
@@ -3,84 +3,54 @@ log.add(log.transports.File, { filename: 'dict.log' });
 
 var fs = require('fs');
 
-fs.readFile("./resources/swedish_sv-ru.dict", 'utf8', function (err,data) {
+fs.readFile("./resources/swedish_sv-ru.json", 'utf8', function (err,data) {
   if (err) {
     return log.error(err);
   }
   
-  var length = data.length;
-  
+  var length = data.length;  
   log.info('dict has %s symbols', length);
+
+  var dict = JSON.parse(data);
+  var complexKeyCount = 0;
   
-  var parser = new Parser();
+  var outDict = {};
   
-  for (var i = 0; i < length; i++) {
-	  var sym = data[i];
-	  
-	  parser.acceptChar(sym);
+  for (var key in dict) {
+	  if (dict.hasOwnProperty(key)) {
+		  if (key.indexOf(' ') !== -1) {
+			  var value = dict[key];
+			  var parts = key.split(' ');
+			  
+			  var newKey = parts[0];
+			  var goodPartsCount = parts.length;
+			  
+			  for (var i = 1; i < parts.length; i++) {
+				  var part = parts[i];
+				  if (part == '' || part == 'ngn' || part == 'ngt' || part == 'ngnngt' || part == 'arsvamp'
+						|| part == 'en' || part == 'et'						
+						|| part == 'konj' || part == 'prep' || part == 'v' || part == 'el'
+						|| part == 'adv' || part == 's' || part == 'a' || part == 'pron' || part == 'def'
+						|| part == '1' || part == '2' || part == '3' || part == '4'
+						|| value.indexOf('pl.') !== -1
+						|| value.indexOf('-' + part) !== -1
+						|| (value.indexOf('-') !== -1 && part.indexOf('er') == part.length - 2)
+						) {
+					  goodPartsCount--;
+				  } else {
+					  newKey += " " + part;
+				  }
+			  }
+			  
+			  if (goodPartsCount > 1) {
+				  complexKeyCount++;
+				  outDict[newKey] = value;
+			  }
+		  }			  
+	  }
   }
   
-  log.info('keys: %s', parser.inKeyCount);
-  log.info('values: %s', parser.waitKeyCount);
+  fs.writeFile("./resources/phrases.json", JSON.stringify(outDict));
+
+  log.info('dict has %s keys', complexKeyCount);
 });
-
-Parser = function () {
-	this.state = 'waitKey';
-	this.currentChar;
-	this.inKeyCount = 0;
-	this.waitKeyCount = 0;
-}
-
-Parser.prototype.acceptChar = function(c) {
-	this.currentChar = c;
-	
-	if (this.state == 'waitKey') {
-		if (c == '<') {
-			this.state = 'key<';
-		}
-	} else if (this.state == 'key<') {
-		if (c == 'k') {
-			this.state = 'keyk';
-		} else {
-			log.error('sequence breaked', this);
-			this.state = 'waitKey';
-		}
-	} else if (this.state == 'keyk') {
-		if (c == '>') {
-			this.inKeyCount++;
-			this.state = 'inKey';
-		} else {
-			log.error('sequence breaked', this);
-			this.state = 'waitKey';
-		}
-	} else if (this.state == 'inKey') {
-		if (c == '<') {
-			this.state = 'inCloseKey<';
-		}
-	} else if (this.state == 'inCloseKey<') {
-		if (c == '/') {
-			this.state = 'inCloseKey/';
-		} else if (c == 'k') {
-			log.error('key opened when need to be closed', this);
-			this.state = 'keyk';
-		} else {
-			log.error('sequence breaked', this);
-			this.state = 'waitKey';
-		}		
-	} else if (this.state == 'inCloseKey/') {
-		if (c == 'k') {
-			this.state = 'inCloseKeyk';
-		} else {
-			log.error('sequence breaked', this);
-			this.state = 'waitKey';
-		}
-	} else if (this.state == 'inCloseKeyk') {
-		if (c == '>') {
-			this.waitKeyCount++;
-			this.state = 'waitKey';
-		} else {
-			log.error('sequence breaked', this);
-			this.state = 'waitKey';
-		}
-	}
-}
